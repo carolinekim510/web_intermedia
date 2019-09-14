9954e3e5b18d42a51c2055772ee4c3df6989a163
carolinekim510 <47015384+carolinekim510@users.noreply.github.com>
2019-09-14 12:01:33 -0500

    Assignment 03
    First upload -work in progress

---
+++ email.php
@@ -0,0 +1,12 @@
+/*
+****************************************************
+Name: Caroline Kim
+Assignment: 03
+Purpose: Create a contact form with proper client side validation
+Notes: Part 1
+*********************************************************
+*/
+
+<?php
+// this will simply dump the posted variables back to the browser
+var_dump($_POST);
